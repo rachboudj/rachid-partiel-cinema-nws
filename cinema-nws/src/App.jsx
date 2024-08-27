ca0a062f023acec21b197d3d2663670626810247
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-08-27 11:44:51 +0200

    filtre par genre de film

--- cinema-nws/src/App.jsx
+++ cinema-nws/src/App.jsx
@@ -17,22 +17,26 @@ function App() {
   const [movies, setMovies] = useState([]);
   const [currentPage, setCurrentPage] = useState(1)
   const [totalPages, setTotalPages] = useState(1);
+  const [selectedGenre, setSelectedGenre] = useState(null);
 
   useEffect(() => {
     const fetchMovies = async () => {
-        try {
-          const response = await axios.get(
-            `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=fr&page=${currentPage}`
-          );
-          setMovies(response.data.results.slice(0, 10));
+      try {
+        const genre = selectedGenre ? `&with_genres=${selectedGenre}` : '';
+        console.log(genre)
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&include_adult=false&include_video=false&language=fr&page=${currentPage}&sort_by=popularity.desc${genre}`
+
+        );
+        setMovies(response.data.results.slice(0, 10));
         setTotalPages(response.data.total_pages);
-        } catch (error) {
-          console.error(`Erreur lors de la récupération des films`, error);
-        }
+      } catch (error) {
+        console.error(`Erreur lors de la récupération des films`, error);
+      }
     };
 
     fetchMovies();
-  }, [currentPage]);
+  }, [currentPage, selectedGenre]);
 
   const handlePageChange = (direction) => {
     setCurrentPage((prev) => {
@@ -43,35 +47,50 @@ function App() {
 
   return (
     <>
-          <>
-            <div className='flex flex-wrap'>
-              {movies.map((movie) => (
-                <div key={movie.id}>
-                  <img
-                    src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                    alt={movie.title}
-                  />
-                  <p>{movie.title}</p>
-                </div>
-              ))}
+      <>
+        <div className="genre-select">
+          <label htmlFor="genre">Choisir un genre:</label>
+          <select
+            id="genre"
+            value={selectedGenre || ''}
+            onChange={(e) => setSelectedGenre(e.target.value)}
+          >
+            <option value="">Tous les genres</option>
+            {genres.map((genre) => (
+              <option key={genre.id} value={genre.id}>
+                {genre.name}
+              </option>
+            ))}
+          </select>
+        </div>
+        <div className='flex flex-wrap'>
+          {movies.map((movie) => (
+            <div key={movie.id}>
+              <img
+                src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                alt={movie.title}
+              />
+              <p>{movie.title}</p>
+            </div>
+          ))}
 
-            </div>
-            <div>
-              <button
-                onClick={() => handlePageChange(-1)}
-                disabled={currentPage === 1}
-              >
-                Précédent
-              </button>
-              <span>Page {currentPage} sur {totalPages}</span>
-              <button
-                onClick={() => handlePageChange(1)}
-                disabled={currentPage === totalPages}
-              >
-                Suivant
-              </button>
-            </div>
-          </>
+        </div>
+        <div>
+          <button
+            onClick={() => handlePageChange(-1)}
+            disabled={currentPage === 1}
+          >
+            Précédent
+          </button>
+          <span>Page {currentPage} sur {totalPages}</span>
+          <button
+            onClick={() => handlePageChange(1)}
+            disabled={currentPage === totalPages}
+          >
+            Suivant
+          </button>
+        </div>
+      </>
     </>
   )
 }
