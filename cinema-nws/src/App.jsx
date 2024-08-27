ab8520cc6dfb577be796e3bc002673662d843303
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-08-27 10:47:59 +0200

    re installation de tailwind

--- cinema-nws/src/App.jsx
+++ cinema-nws/src/App.jsx
@@ -6,22 +6,59 @@ const API_KEY = '18ffbdd4f338668948dfeecc71baa949';
 const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=fr-US&page=1`;
 
 function App() {
+  const genres = [
+    { id: 28, name: 'Action' },
+    { id: 12, name: 'Aventure' },
+    { id: 16, name: 'Animation' },
+    { id: 35, name: 'Comédie' },
+    { id: 80, name: 'Crime' },
+  ];
+
+  const [moviesByGenre, setMoviesByGenre] = useState({});
+
   useEffect(() => {
-    const fetchMovies = async () => {
-      try {
-        const response = await axios.get(URL);
-        console.log(response.data.results);
-      } catch (error) {
-        console.error('Erreur lors de la récupération des films:', error);
+    const fetchMoviesByGenre = async () => {
+      const movies = {};
+      for (let genre of genres) {
+        try {
+          const response = await axios.get(
+            `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${genre.id}&language=en-US&page=1`
+          );
+          movies[genre.name] = response.data.results;
+        } catch (error) {
+          console.error(`Erreur lors de la récupération des films pour le genre ${genre.name}:`, error);
+        }
       }
+      setMoviesByGenre(movies);
     };
 
-    fetchMovies();
+    fetchMoviesByGenre();
   }, []);
 
   return (
     <>
-      <h1>hello</h1>
+    <h1 className='text-3xl font-bold underline'>jdskjdfjf</h1>
+      {Object.keys(moviesByGenre).length === 0 ? (
+        <p>Chargement...</p>
+      ) : (
+        genres.map((genre) => (
+          <div key={genre.id}>
+            <h2 className='bg-red-900'>{genre.name}</h2>
+            <div className='flex bg-red-500'>
+              {moviesByGenre[genre.name]?.map((movie) => (
+                <div key={movie.id} style={{ margin: '10px' }}>
+                  <img
+                    src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                    alt={movie.title}
+                    style={{ borderRadius: '8px' }}
+                  />
+                  <p>{movie.title}</p>
+                </div>
+              ))}
+            </div>
+          </div>
+        ))
+      )}
     </>
   )
 }
