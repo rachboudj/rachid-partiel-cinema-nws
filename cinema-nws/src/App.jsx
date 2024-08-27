2d36776a784aeb86ec86e47f38e091efddffcb05
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-08-27 10:56:51 +0200

    affichage des films par genre

--- cinema-nws/src/App.jsx
+++ cinema-nws/src/App.jsx
@@ -2,10 +2,11 @@ import { useState, useEffect } from 'react'
 import './App.css'
 import axios from 'axios';
 
-const API_KEY = '18ffbdd4f338668948dfeecc71baa949';
-const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=fr-US&page=1`;
-
 function App() {
+
+  const API_KEY = '18ffbdd4f338668948dfeecc71baa949';
+  const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=fr-US&page=1`;
+
   const genres = [
     { id: 28, name: 'Action' },
     { id: 12, name: 'Aventure' },
@@ -22,7 +23,7 @@ function App() {
       for (let genre of genres) {
         try {
           const response = await axios.get(
-            `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${genre.id}&language=en-US&page=1`
+            `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=${genre.id}&language=fr&page=1`
           );
           movies[genre.name] = response.data.results;
         } catch (error) {
@@ -37,25 +38,18 @@ function App() {
 
   return (
     <>
-    <h1 className='text-3xl font-bold underline'>jdskjdfjf</h1>
-      {Object.keys(moviesByGenre).length === 0 ? (
-        <p>Chargement...</p>
-      ) : (
+      {(
         genres.map((genre) => (
-          <div key={genre.id}>
-            <h2 className='bg-red-900'>{genre.name}</h2>
-            <div className='flex bg-red-500'>
-              {moviesByGenre[genre.name]?.map((movie) => (
-                <div key={movie.id} style={{ margin: '10px' }}>
-                  <img
-                    src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
-                    alt={movie.title}
-                    style={{ borderRadius: '8px' }}
-                  />
-                  <p>{movie.title}</p>
-                </div>
-              ))}
-            </div>
+          <div key={genre.id} className='flex flex-wrap'>
+            {moviesByGenre[genre.name]?.map((movie) => (
+              <div key={movie.id}>
+                <img
+                  src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}
+                  alt={movie.title}
+                />
+                <p>{movie.title}</p>
+              </div>
+            ))}
           </div>
         ))
       )}
