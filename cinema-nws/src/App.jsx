0888b249cc9775954491cce8f91ead66c346f2e7
Rachid Boujakdji <rachidboudjakdji27@gmail.com>
2024-08-27 10:31:45 +0200

    test API

--- cinema-nws/src/App.jsx
+++ cinema-nws/src/App.jsx
@@ -1,8 +1,23 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
+import axios from 'axios';
+
+const API_KEY = '18ffbdd4f338668948dfeecc71baa949';
+const URL = `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&language=fr-US&page=1`;
 
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get(URL);
+        console.log(response.data.results);
+      } catch (error) {
+        console.error('Erreur lors de la récupération des films:', error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
 
   return (
     <>
